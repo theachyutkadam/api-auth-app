64cbf479af606764d26b7372f595c5421639baf4
achyut kadam <theachyutkadam@gmail.com>
2022-06-09 16:21:00 +0530

    Added user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,28 +4,15 @@ RSpec.describe User, type: :model do
   context '#create' do
     let(:user) { create(:user) }
     it 'should create new user' do
-      user.save
       expect(user).to be_valid
     end
-
-    # it 'should give error message if username is empty' do
-    #   user = build(:user, username: '')
-    #   user.save
-    #   expect(user.errors.messages[:username].first).to eq("can't be blank")
-    # end
   end
 
-  # context 'authenticate' do
-  #   it 'should not authenticate if credentials are invalid' do
-  #     is_valid = User.authenticate('invalid_username', 'invalid_password')
-  #     expect(is_valid).to be_falsey
-  #   end
-
-  #   it 'should authenticate if credentials are valid' do
-  #     user = build(:user, username: 'username', password: '123456')
-  #     user.save
-  #     is_valid = User.authenticate('username', '123456')
-  #     expect(is_valid).to be_truthy
-  #   end
-  # end
+  context '#validation' do
+    let(:user_without_email) { build(:user, email: '') }
+    it 'return error message if email is empty' do
+      user_without_email.save
+      expect(user_without_email.errors.messages[:email].first).to eq("can't be blank")
+    end
+  end
 end
